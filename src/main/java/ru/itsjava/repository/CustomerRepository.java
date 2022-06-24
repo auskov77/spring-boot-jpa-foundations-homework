b12552429a30dd9b69bf032a4362e4ea3c6e92b8
auskov77 <auskov77@yandex.ru>
2022-06-24 12:04:24 +0300

    Spring Data

---
+++ src/main/java/ru/itsjava/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package ru.itsjava.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.itsjava.domain.Customer;
+import ru.itsjava.domain.Pet;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findCustomerByNameAndPet(String name, Pet pet);
+}
