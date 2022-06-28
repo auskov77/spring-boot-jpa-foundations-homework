64919d1ac78e3374bd4b428226ebc2e9b9eb2f27
auskov77 <auskov77@yandex.ru>
2022-06-28 21:12:38 +0300

    Spring MVC. Пишем изменение и удаление сущности

--- src/main/java/ru/itsjava/services/CustomerService.java
+++ src/main/java/ru/itsjava/services/CustomerService.java
@@ -1,10 +1,20 @@
 package ru.itsjava.services;
 
 import ru.itsjava.domain.Customer;
+import ru.itsjava.domain.Pet;
 
 import java.util.List;
 
 public interface CustomerService {
     void printAllCustomers();
+
     List<Customer> getAllCustomers();
+
+    Customer getCustomerById(long id);
+
+    void createCustomer(Customer customer);
+
+    void updateCustomer(Customer customer);
+
+    void deleteCustomer(Customer customer);
 }
